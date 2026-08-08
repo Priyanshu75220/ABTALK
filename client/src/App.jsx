7fd8e401b0f772cb581f8906e96bd8f4bac6e58e
priyanshu maurya <priyanshumaurya@priyanshus-MacBook-Air-5.local>
2026-08-09 00:25:14 +0530

    App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Landing from './pages/Landing'
+import Dashboard from './pages/Dashboard'
+import ChallengeDay from './pages/ChallengeDay'
+
+const App = () => {
+
+  return (
+    <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/day/12" element={<ChallengeDay />} />
+    </Routes>
+  )
+}
+
+export default App
